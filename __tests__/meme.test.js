0270f8cf3c456a03add166d4a74442a63acdc416
Josh Ford <thisisjoshford@gmail.com>
2020-04-12 21:01:47 -0700

    create a meme route complete!

--- __tests__/meme.test.js
+++ __tests__/meme.test.js
@@ -6,17 +6,17 @@ describe('meme routes', () => {
   it('creates a meme', () => {
 
     return request(app)
-      .post('/api/v1/meme')
+      .post('/api/v1/memes')
       .send({
         top: 'I don\'t always code...',
-        url: 'http://www.memeguy.com/image.jpg',
+        image: 'http://www.memeguy.com/image.jpg',
         bottom: '...but when I do... I have a dog named spot.'
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           top: 'I don\'t always code...',
-          url: 'http://www.memeguy.com/image.jpg',
+          image: 'http://www.memeguy.com/image.jpg',
           bottom: '...but when I do... I have a dog named spot.',
           __v: 0
         });
